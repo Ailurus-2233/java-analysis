52f9398e6d5c23ccda25848e36923025040343d5
ailurus <ailurus2233@outlook.com>
2022-12-12 10:40:49 +0800

    TPL_Download v0.1

--- java-analysis-main/src/main/java/pers/ailurus/Main.java
+++ java-analysis-main/src/main/java/pers/ailurus/Main.java
@@ -1,19 +1,36 @@
 package pers.ailurus;
 
+import cn.hutool.core.text.csv.CsvRow;
 import com.google.devtools.common.options.OptionsParser;
 import pers.ailurus.args.Options;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        args = new String[]{"--jar_file", "a", "--output_file", "b"};
+        args = new String[]{"--csv_file", "C:\\Users\\ailur\\Desktop\\log4j.csv", "--output_folder", "C:\\Users\\ailur\\Desktop\\temp"};
 //        args = new String[]{"--help"};
         OptionsParser parser = OptionsParser.newOptionsParser(Options.class);
         parser.parseAndExitUponError(args);
         Options options = parser.getOptions(Options.class);
-        System.out.printf("%s\n%s\n%s", options.jarFile, options.outputFile, options.netFile);
+
+        try {
+            List<CsvRow> rows = MyFileUtil.readCSVFile(options.csvFile);
+            for (CsvRow row : rows) {
+                if (row.getOriginalLineNumber() == 0) continue;
+                String fileName = String.format("%s-%s.jar", row.getRawList().get(0), row.getRawList().get(1));
+                String url = row.getRawList().get(2);
+                NetUtil.download(url, options.outputFolder + File.separator + fileName);
+//                System.out.println(fileName);
+//                System.out.println(url);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
         // 结束程序
--- java-analysis-main/src/main/java/pers/ailurus/args/Options.java
+++ java-analysis-main/src/main/java/pers/ailurus/args/Options.java
@@ -5,10 +5,8 @@ import com.google.devtools.common.options.OptionsBase;
 
 
 public class Options extends OptionsBase {
-    @Option(name = "jar_file", help = "待分析的Jar文件路径", defaultValue = "")
-    public String jarFile;
-    @Option(name = "output_file", help = "结果输出路径", defaultValue = "")
-    public String outputFile;
-    @Option(name = "net_file", help = "使用url下载待分析Jar文件", defaultValue = "")
-    public String netFile;
+    @Option(name = "csv_file", help = "待下载TPL的CSV文件", defaultValue = "")
+    public String csvFile;
+    @Option(name = "output_folder", help = "结果输出路径", defaultValue = "")
+    public String outputFolder;
 }
